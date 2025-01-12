4f30de9c4abded6fe508cea8bbe2a51cc309b8a6
susharasuresh <122563023+susharasuresh@users.noreply.github.com>
2025-01-12 18:47:15 +0530

    Add files via upload

---
+++ inverted full pyramid star.c
@@ -0,0 +1,28 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    
+    int n=5;
+    for (int i=0; i<n; i++) {
+        
+        for (int j=0; j<2*i; j++) {
+            printf(" ");
+        }
+        
+        for (int k=0; k<2*(n-i)-1; k++) {
+            printf("* ");
+        }
+        printf("\n");
+    }
+
+    return 0;
+}
