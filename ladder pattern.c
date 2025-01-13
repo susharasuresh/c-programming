74cffb2984e52af783d189e6ae11085b005648aa
susharasuresh <122563023+susharasuresh@users.noreply.github.com>
2025-01-13 19:27:10 +0530

    Add files via upload

---
+++ ladder pattern.c
@@ -0,0 +1,34 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main()
+{
+    
+    int n;
+    printf("enter a number:");
+    scanf("%d",&n);
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=2*i;j++){
+            printf("* ");
+        }
+        printf("\n");
+        for(int k=1;k<=3*i;k++){
+            if(i==n){
+                break;
+            }
+           printf("* \n");
+           
+        }
+        
+    }
+    
+
+    return 0;
+}
